36f38e58fb10625dece50f993836d1c53dc9627c
julian pulecio <developer@julianpulecio.com>
2023-08-22 23:42:22 -0500

    example of model with text choices

---
+++ model_with_choices_field/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ModelWithChoicesFieldConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'model_with_choices_field'
---
+++ model_with_choices_field/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    class Gender(models.TextChoices):
+        MALE = ('M','Male')
+        FEMALE = ('F','Female')
+        
+    name = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    gender = models.CharField(choices=Gender.choices)
+
+    def __str__(self):
+        return self.name
